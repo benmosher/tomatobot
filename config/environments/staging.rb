4976dbfbaea2fa2fa69aa92e0e3fa8a118841330
Matthew Higgins <matthew@matthewhiggins.me>
2016-03-06 18:48:11 +0200

    Remove SMTP

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,10 +1,6 @@
 require_relative "production"
 Rails.application.routes.default_url_options[:host] = ENV.fetch("APPLICATION_HOST")
 
-Mail.register_interceptor(
-  RecipientInterceptor.new(ENV.fetch("EMAIL_RECIPIENTS"))
-)
-
 Rails.application.configure do
   # ...
 
