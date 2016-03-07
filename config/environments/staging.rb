4d06bd27b922e153149af07b34fd8eacb4a0f81e
Matthew Higgins <matthew@matthewhiggins.me>
2016-03-07 19:25:14 +0200

    Log everything in staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -2,7 +2,6 @@ require_relative "production"
 Rails.application.routes.default_url_options[:host] = ENV.fetch("APPLICATION_HOST")
 
 Rails.application.configure do
-  # ...
-
+  config.log_level = :debug
   config.action_mailer.default_url_options = { host: ENV.fetch("APPLICATION_HOST") }
 end
